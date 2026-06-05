b4672f168f10c674050f753bae69122a609cf441
Toby Anand <toby.anand8@gmail.com>
2026-06-05 20:35:50 +0100

    Update YouTube video to correct link

--- app/page.tsx
+++ app/page.tsx
@@ -217,7 +217,7 @@ export default function Home() {
                   <iframe
                     width="100%"
                     height="100%"
-                    src="https://www.youtube.com/embed/7lki8o-aXuY"
+                    src="https://www.youtube.com/embed/AOS8YMTZy4U"
                     title="AI Operators Collective Introduction"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
